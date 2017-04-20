deba6d14de75464acdc5c34bca9971a09e6fa139
andy-shi88 <shi88.andy@gmail.com>
2017-04-20 12:06:55 +0700

    add boilerplate off the apps

--- app/components/Main/index.js
+++ app/components/Main/index.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, StyleSheet, Text, TextInput, Button, View, ScrollView} from 'react-native';
 import { Router, Scene } from 'react-native-router-flux';
+import Styles from './styles';
+
 
 export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      link: ''
+    };
+  }
+  setLink(text) {
+    this.setState({
+      link: text
+    });
+  }
   render() {
     return (
-      <View>
-        <Text>Main scene</Text>
+      <View style={Styles.container}>
+        <View style={Styles.conversation}>
+          <ScrollView style={Styles.messageList}>
+
+          </ScrollView>
+        </View>
+      <View style={Styles.inputController}>
+          <TextInput
+            style={Styles.messageInput}
+            placeholder="your message here"
+            onChangeText={(text) => this.setLink(text)}
+            blurOnSubmit={true}
+            multiline={false}
+            >
+          </TextInput>
+          <Button
+            style={Styles.buttonSend}
+            title= "Send"
+            // onPress={this.sendLink.bind(this)}
+          />
+        </View>
       </View>
     );
   }
