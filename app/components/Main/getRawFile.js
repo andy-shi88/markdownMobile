c813f4ccdb274964fd5e68831a5bcfbb51577a39
andy-shi88 <shi88.andy@gmail.com>
2017-04-20 17:23:42 +0700

    deleted junk code

--- app/components/Main/getRawFile.js
+++ app/components/Main/getRawFile.js
@@ -1,34 +1,18 @@
-
-const getMd = getMDAsync(repo) {
-  return fetch('https://api.github.com/repos/'+repo+'/contents/')
-  .then((response) => response.json())
-  .then((responseJson) => {
-    // debuging
-    //console.log(responseJson);
-    var tes=/.md/;
-    for(var i= 0; i < responseJson.length; i++){
-      //debuging
-      //console.log(responseJson[i].download_url)
-      if(tes.test(responseJson[i].download_url) == true){
-        return responseJson[i].download_url
-        // console.log(responseJson[i].download_url)
+let getRawFile = {
+    getFile(repo) {
+      return fetch('https://api.github.com/repos/'+repo+'/contents/')
+      .then((response) => response.json())
+      .then((responseJson) => {
+      var tes=/.md/;
+      for(var i= 0; i < responseJson.length; i++){
+        if(tes.test(responseJson[i].download_url) == true){
+          return responseJson[i].download_url
+          }
       }
-    }
-  })
-  .catch((error) => {
-    console.log(error);
-  })
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  },
 }
-
-
-// examle for code js
-
-// getMDAsync('lwansbrough/react-native-markdown')
-//   .then((responseJson) => {
-//     console.log(responseJson);
-//   }).catch((error) => {
-//     console.log(error);
-//   })
-// ;
-
-export default getMD;
+module.exports =  getRawFile;
