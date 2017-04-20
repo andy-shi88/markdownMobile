da64049c6a0e527785fb2b1499983cedb373834a
adityawira15 <aditya_wira98@yahoo.com>
2017-04-20 17:52:14 +0700

    delete comment

--- app/components/Main/getRawFile.js
+++ app/components/Main/getRawFile.js
@@ -3,15 +3,10 @@ const getMd = getMDAsync(repo) {
   return fetch('https://api.github.com/repos/'+repo+'/contents/')
   .then((response) => response.json())
   .then((responseJson) => {
-    // debuging
-    //console.log(responseJson);
     var tes=/.md/;
     for(var i= 0; i < responseJson.length; i++){
-      //debuging
-      //console.log(responseJson[i].download_url)
       if(tes.test(responseJson[i].download_url) == true){
         return responseJson[i].download_url
-        // console.log(responseJson[i].download_url)
       }
     }
   })
