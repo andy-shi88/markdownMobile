224b03c196dc5596a3d63cfd4b728929626e4003
AryaUP <arya.ddg00@gmail.com>
2017-04-20 15:25:19 +0800

    add parsermMrkdown.js

---
+++ app/components/ReadMD/parseMarkdown.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class parseMarkdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      link: 'props.link';
+    };
+  }
+  componentWillMount()
+  render() {
+    return (
+      <View>
+        <Text>Readme here</Text>
+      </View>
+    );
+  }
+}
