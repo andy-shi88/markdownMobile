cbcf6b99efb9a21728633a9784b008f7a4ab9863
AryaUP <arya.ddg00@gmail.com>
2017-04-20 15:54:26 +0800

    markdown

--- app/components/ReadMD/parseMarkdown.js
+++ app/components/ReadMD/parseMarkdown.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { View } from 'react-native';
+import Markdown from 'react-native-markdown';
 
 export default class parseMarkdown extends Component {
   constructor(props) {
@@ -7,11 +9,22 @@ export default class parseMarkdown extends Component {
       link: 'props.link';
     };
   }
-  componentWillMount()
+
   render() {
     return (
       <View>
-        <Text>Readme here</Text>
+        <Markdown>
+          {
+            fetch('https://raw.githubusercontent.com/lwansbrough/react-native-markdown/master/README.md')
+            .then((res) => {return res.json()})
+            .then((resData) => {
+              return resData;
+            })
+            .catch((error) => {
+              console.log(error);
+            })
+          }
+        </Markdown>
       </View>
     );
   }
