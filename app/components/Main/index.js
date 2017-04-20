330d26ec1750d9e27b9141756f9b121edbb311a3
andy-shi88 <shi88.andy@gmail.com>
2017-04-20 11:55:42 +0700

    set everything up

---
+++ app/components/Main/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { Router, Scene } from 'react-native-router-flux';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Main scene</Text>
+      </View>
+    );
+  }
+}
